a4f72c214708bc228b5e0d693f1f18b322ce57eb
eric <ericblancas23@gmail.com>
2019-05-29 00:37:53 -0700

    inheritance and initializing functions

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
+import lexer
+
 def main():
     content = ""
     with open('test.lang', 'r') as file:
         content = file.read()
     print(content)
 
+    lex = lexer.Lexer(content)
+    tokens = lex.tokenize()
 main()
